b1583acaa082d2852fcdb568170f7b539fd3cbb4
hariomm22 <hariommahajan22@gmail.com>
2025-04-30 13:04:39 +0530

    database configuration

---
+++ src/main/java/com/example/BlogAPI/BlogApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.BlogAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiApplication.class, args);
+		System.out.println("Server is running..!");
+	}
+
+}
